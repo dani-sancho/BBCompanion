b004f35266dd343a214a3bb98c683686ca60b606
dani-sancho <d.sancho.23@hotmail.com>
2026-06-26 01:10:00 +0200

    feat: final item-display style

--- src/app/presentation/components/item-display/item-display.interface.ts
+++ src/app/presentation/components/item-display/item-display.interface.ts
@@ -1,13 +1,17 @@
+import { itemIcon } from './item-display.constants';
+
 export interface itemDisplayConfig {
-    id: string;
-    name: string;
-    category: string;
-    dueDate: Date;
+  id: string;
+  name: string;
+  category: string;
+  dueDate: Date;
+  dateStatus: number;
+  icon?: itemIcon;
 }
 
 export const itemTypes = {
-    list: 'list',
-    block: 'block'
+  list: 'list',
+  block: 'block',
 } as const;
 
-export type itemType = typeof itemTypes[keyof typeof itemTypes];
+export type itemType = (typeof itemTypes)[keyof typeof itemTypes];
