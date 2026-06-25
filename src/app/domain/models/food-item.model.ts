6b40e33116baf36fd61212e6b0be537a0aabfe3c
dani-sancho <d.sancho.23@hotmail.com>
2026-06-26 00:27:33 +0200

    refactor: add expiry status on food-item implemntation object

--- src/app/domain/models/food-item.model.ts
+++ src/app/domain/models/food-item.model.ts
@@ -1,25 +1,26 @@
 export interface FoodItem {
-    id: number;
-    name: string;
-    category: Category;
-    brand: Brand;
-    location: Location;
-    quantity: number;
-    bestBefore: Date;
-    openedDate?: Date;
+  id: number;
+  name: string;
+  category: Category;
+  brand: Brand;
+  location: Location;
+  quantity: number;
+  bestBefore: Date;
+  openedDate?: Date;
+  dateStatus?: number;
 }
 
 export interface Category {
-    name?: string;
-    id?: number;
+  name?: string;
+  id?: number;
 }
 
 export interface Brand {
-    name?: string;
-    id?: number;
+  name?: string;
+  id?: number;
 }
 
 export interface Location {
-    name?: string;
-    id?: number;
+  name?: string;
+  id?: number;
 }
